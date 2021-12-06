8bd2c6b9df99c68ebebfe6e9b975d0d3bb1f9440
HasnainM <69258165+Hassi34@users.noreply.github.com>
2021-12-06 17:08:01 +0000

    .

--- scraper.py
+++ scraper.py
@@ -1,1 +1,18 @@
-print('Hello world')
+import requests 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+response = requests.get(YOUTUBE_TRENDING_URL)
+
+with open('trending.html', 'w' ) as f :
+  f.write(response.text)
+
+doc = BeautifulSoup(response.text, 'html.parser')
+print('Page title : ', doc.title.text)
+
+#Find all the video divs
+video_divs = doc.find_all('div', class_ = 'ytd-video-renderer')
+print(len(video_divs))
+
+driver = webdriver.Chrome()
+driver.gwt(YOUTUBE_TRENDING_URL)
